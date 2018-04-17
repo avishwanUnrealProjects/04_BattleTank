2d5c0efec84ab613b180e184fe3e5d472a5aaf98
DESKTOP-6FFKJ74\aaron <aaron.narain@gmail.com>
2018-04-16 21:13:49 -0400

    C++ classes for playercontroller and pawn and reparented blueprint classes to have those as parent. Adjusted input ui to game ui once battleground level is opened, in BP so that mouse movement controls tank.

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto possessed = GetControlledTank();
+	if (possessed)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessing %s"), *(possessed->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Pawn!"));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play!"));
+}
+
+
